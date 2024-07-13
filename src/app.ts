9070f5f5ce8803d3e509681301844bbb64876286
faysalahmed000012 <faysal000012@gmail.com>
2024-07-14 00:33:27 +0600

    cors-4\

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,7 @@ import router from "./app/product.routes";
 const app: Application = express();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-    allowedHeaders: "Origin,X-Requested-With,Content-Type,Accept,Authorization",
-  })
-);
+app.use(cors());
 
 app.use("/products", router);
 
