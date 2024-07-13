28e31bcfd1e6593ef2be3e7dcd4034e31027c11d
faysalahmed000012 <faysal000012@gmail.com>
2024-07-14 00:08:34 +0600

    cors-2

--- src/app.ts
+++ src/app.ts
@@ -7,11 +7,10 @@ const app: Application = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: [
-      "https://4-7kgjwt6ra-faysalahmed000012s-projects.vercel.app/",
-      "*",
-    ],
+    origin: "https://4-7kgjwt6ra-faysalahmed000012s-projects.vercel.app",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
+    allowedHeaders: "Origin,X-Requested-With,Content-Type,Accept,Authorization",
   })
 );
 
